4044edabbe51951b39d4eb2cf6fb11e4ff7f7a0b
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-10-25 00:38:55 -0300

    Componente Loader

---
+++ bp4-front/src/components/LoadWrapper.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import ProgressSpinner from 'primevue/progressspinner'
+import { computed } from 'vue'
+import { useAppStore } from '@/stores/app'
+
+const appStore = useAppStore()
+
+const isLoading = computed(() => appStore.loading)
+const error = computed(() => appStore.error)
+</script>
+
+<template>
+  <div class="wrapper">
+    <div v-if="isLoading" class="wrapper-loader">
+      <ProgressSpinner />
+    </div>
+    <div v-else-if="error" class="wrapper-error">
+      <h1>Algo ocurrió</h1>
+      <p>{{ error }}</p>
+    </div>
+    <slot v-else />
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper-loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
