9bc034e6cde24fec799006c240587fd2284d9554
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-10-26 14:07:57 -0300

    eliminio dist

--- bp4-back/dist/src/utils/db.server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-let prisma;
-if (!global.prisma) {
-    global.prisma = new client_1.PrismaClient();
-}
-prisma = global.prisma;
-exports.default = prisma;
